87d104e3f9f92978231d36347ee2b7ff0e48f16b
kraky <kraky@net.hr>
2016-04-22 09:56:18 +0200

    Modifying the Index View to Provide a GUI for Creating Contacts

--- Kreni/KontaktMenager/Controllers/ContactController.cs
+++ Kreni/KontaktMenager/Controllers/ContactController.cs
@@ -23,5 +23,14 @@ namespace KontaktMenager.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
